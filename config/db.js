2ecdb443d03abdce68ce8747f08736592082ce21
iamthampuran <amoghsthampuran@gmail.com>
2022-09-20 07:51:11 +0530

    cha

--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-require('./../.env')
 
-mongoose.connect(MONGODB_URI,{
+mongoose.connect(process.env.MONGODB_URI,{
     useNewUrlParser: true, 
     useUnifiedTopology:true,
 })
