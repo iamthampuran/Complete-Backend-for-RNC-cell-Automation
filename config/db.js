f98f592c61dec0c5ff2db942fa9988f0b018052e
iamthampuran <amoghsthampuran@gmail.com>
2022-09-20 07:58:39 +0530

    trying db

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-//require('./../.env')
+
 
 mongoose.connect("mongodb+srv://Amogh:ASTDB@cluster0.myclosi.mongodb.net/?retryWrites=true&w=majority",{
     useNewUrlParser: true, 
